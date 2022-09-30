a2832b89e09a4a11f766f29efbaffaa8b883b626
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-09-30 12:28:21 +0500

    Выполнение AJAX POST запросов с помощью Django и JQuery

---
+++ dj_ajax/ajax/forms.py
@@ -0,0 +1,9 @@
+# ajax/forms.py
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
---
+++ dj_ajax/ajax/urls.py
@@ -0,0 +1,18 @@
+# ajax/urls.py
+from django.urls import path
+from .views import home, SignUpView, validate_username
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('signup', SignUpView.as_view(), name='signup'),
+    path('validate_username', validate_username, name='validate_username')
+]
+
+# dj_ajax/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ajax/', include('ajax.urls'))
+]
